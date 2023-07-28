1e15218673293102b00a9a11601596d9006b32d9
Jal <109842448+jal1306@users.noreply.github.com>
2023-07-28 11:48:00 +0530

    Add files via upload

--- Qus5.java
+++ Qus5.java
@@ -6,8 +6,12 @@ public class Qus5
 {
 	public static void main(String args[])
 	{
-        String str = "Jal Majithiya";
-        char ch = 'i';
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String:- ");
+        String str = sc.nextLine();
+		System.out.println("Enter Character to find occurrence:- ");
+        char ch = sc.next().charAt(0);
+        //char ch = 'i';
         int cnt = 0;
          
         for ( int i = 0; i < str.length(); i++) {
